edf6b902377f18d7c33cc00c262b4d73664aeb6f
Carlos López <carloslopez0519@gmail.com>
2023-06-06 23:55:26 +0200

    utils mofify

--- src/pages/ResultsPage.tsx
+++ src/pages/ResultsPage.tsx
@@ -1,8 +1,8 @@
-import { useState } from "react";
-import FloatingButton from "../components/FloatingButton";
-import { Modal } from "../components/Modal";
-import { useQuestionsStore } from "../store/useQuestionsStore";
-import { questions } from "../utils/questions";
+import { useState } from 'react';
+import FloatingButton from '../components/FloatingButton';
+import { Modal } from '../components/Modal';
+import { useQuestionsStore } from '../store/useQuestionsStore';
+import { questions } from '../utils/questions';
 
 export const ResultPage = () => {
   const [showModal, setShowModal] = useState(false);
@@ -17,28 +17,28 @@ export const ResultPage = () => {
     setShowModal(false);
   };
 
-  console.log("questions");
+  console.log('questions');
   console.table(questions);
-  console.log("userAnswers");
+  console.log('userAnswers');
   console.table(userAnswers);
 
   const message = () => {
     let message;
     if (percentageCorrect < 25) {
       message =
-        "Aún necesitas mucho por aprender. ¡No te desanimes! Sigue practicando y verás cómo mejoras.";
+        'Aún necesitas mucho por aprender. ¡No te desanimes! Sigue practicando y verás cómo mejoras.';
     } else if (percentageCorrect >= 25 && percentageCorrect < 50) {
       message =
-        "Vas por buen camino, pero aún hay margen de mejora. Continúa estudiando y practicando para obtener mejores resultados.";
+        'Vas por buen camino, pero aún hay margen de mejora. Continúa estudiando y practicando para obtener mejores resultados.';
     } else if (percentageCorrect >= 50 && percentageCorrect < 70) {
       message =
-        "¡Excelente progreso! Estás obteniendo buenas respuestas, pero aún puedes mejorar. Sigue esforzándote.";
+        '¡Excelente progreso! Estás obteniendo buenas respuestas, pero aún puedes mejorar. Sigue esforzándote.';
     } else if (percentageCorrect >= 70 && percentageCorrect < 100) {
       message =
-        "¡Muy bien! Estás cerca de alcanzar un gran nivel. Sigue así y pronto llegarás al dominio completo del tema.";
+        '¡Muy bien! Estás cerca de alcanzar un gran nivel. Sigue así y pronto llegarás al dominio completo del tema.';
     } else if (percentageCorrect === 100) {
       message =
-        "¡Felicitaciones! Has respondido correctamente todas las preguntas. ¡Eres un experto en este tema!";
+        '¡Felicitaciones! Has respondido correctamente todas las preguntas. ¡Eres un experto en este tema!';
     }
 
     return message;
@@ -46,88 +46,88 @@ export const ResultPage = () => {
 
   const checkIsCorrect = (id: string) => {
     const finded = userAnswers.find((answer) => answer.id === id);
-    if (!finded) return "border-transparent";
-    return finded?.isCorrect ? "border-green-400" : "border-red-400";
+    if (!finded) return 'bg-white';
+    return finded?.isCorrect ? 'bg-emerald-100' : 'bg-red-200';
   };
 
   return (
-    <div className='flex flex-col bg-gradient-to-tr from-amber-300 to-yellow-600 px-5 md:px-60'>
-      <div className='flex items-center justify-between mt-8'>
-        <p className='text-5xl md:text-6xl font-bold text-white mb-2 drop-shadow-md'>
+    <div className="flex flex-col bg-gradient-to-tr from-amber-300 to-yellow-600 px-5 md:px-60">
+      <div className="flex items-center justify-between mt-8">
+        <p className="text-5xl md:text-6xl font-bold text-white mb-2 drop-shadow-md">
           Resultado
         </p>
         <svg
-          xmlns='http://www.w3.org/2000/svg'
-          fill='none'
-          viewBox='0 0 24 24'
-          strokeWidth='1.5'
-          stroke='currentColor'
-          className='w-10 h-10 text-white drop-shadow-md hover:cursor-pointer hover:text-stone-300 transition-all'
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-10 h-10 text-white drop-shadow-md hover:cursor-pointer hover:text-stone-300 transition-all"
           onClick={handleOpenModal}
         >
           <path
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            d='M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25'
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M3.75 6.75h16.5M3.75 12h16.5M12 17.25h8.25"
           />
         </svg>
       </div>
 
-      <p className='text-1xl md:text-xl text-white'>
-        <span className='text-2xl md:text-2xl font-bold text-slate-900'>
-          {punctuation}{" "}
+      <p className="text-1xl md:text-xl text-white">
+        <span className="text-2xl md:text-2xl font-bold text-slate-900">
+          {punctuation}{' '}
         </span>
-        {punctuation === 1 ? "respuesta correcta " : "respuestas correctas "}
+        {punctuation === 1 ? 'respuesta correcta ' : 'respuestas correctas '}
         de {questions.length}
       </p>
       <hr />
-      <p className='md:text-2xl font-bold text-slate-900 my-6'>{message()}</p>
+      <p className="md:text-2xl font-bold text-slate-900 my-6">{message()}</p>
 
       <div
         className={`flex items-center justify-center w-full p-5 transition-all rounded-lg bg-teal-600 mb-6 `}
       >
-        <div className='text-sm font-bold'>Revisa tus respuestas</div>
+        <div className="text-sm font-bold">Revisa tus respuestas</div>
         <svg
-          xmlns='http://www.w3.org/2000/svg'
-          fill='none'
-          viewBox='0 0 24 24'
-          strokeWidth='1.5'
-          stroke='currentColor'
-          className='w-6 h-6 ml-2 top-0 left-0 animate-bounce'
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth="1.5"
+          stroke="currentColor"
+          className="w-6 h-6 ml-2 top-0 left-0 animate-bounce"
         >
           <path
-            strokeLinecap='round'
-            strokeLinejoin='round'
-            d='M12 4.5v15m0 0l6.75-6.75M12 19.5l-6.75-6.75'
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 4.5v15m0 0l6.75-6.75M12 19.5l-6.75-6.75"
           />
         </svg>
       </div>
 
       {questions.map((question) => (
-        <div key={question.title} className='mb-3'>
-          <p className='mb-2 font-semibold'>{question.title}</p>
+        <div key={question.title} className="mb-3">
+          <p className="mb-2 font-semibold">{question.title}</p>
           {question.options.map((q) => (
             <div
-              className={`items-center justify-between w-full p-5 transition-all rounded-lg mb-3 border-2 ${checkIsCorrect(
+              className={`items-center justify-between w-full p-5 transition-all rounded-lg mb-3 ${checkIsCorrect(
                 q.id
               )}`}
               key={q.id}
             >
-              <div className='flex'>
-                <div className='w-full text-sm'>{q.textoRespuesta}</div>
+              <div className="flex">
+                <div className="w-full text-sm">{q.textoRespuesta}</div>
                 {q.isCorrect && (
                   <svg
-                    xmlns='http://www.w3.org/2000/svg'
-                    fill='none'
-                    viewBox='0 0 24 24'
-                    strokeWidth='1.5'
-                    stroke='currentColor'
-                    className='w-6 h-6 text-green-400'
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    strokeWidth="1.5"
+                    stroke="currentColor"
+                    className="w-6 h-6 text-green-400"
                   >
                     <path
-                      strokeLinecap='round'
-                      strokeLinejoin='round'
-                      d='M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z'
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
                     />
                   </svg>
                 )}
--- src/utils/questions.ts
+++ src/utils/questions.ts
@@ -1,151 +1,205 @@
-import { Question } from "../interfaces/quest";
+import { Question } from '../interfaces/quest';
 
 export const questions: Question[] = [
   {
-    id: "1",
-    title: "¿Donde se ejecuta Javascript?",
+    id: '1',
+    title: '¿Donde se ejecuta Javascript?',
     options: [
-      { id: "resp-1", textoRespuesta: "Lado del cliente", isCorrect: true },
-      { id: "resp-2", textoRespuesta: "Servidor", isCorrect: false },
-      { id: "resp-3", textoRespuesta: "BIOS", isCorrect: false },
+      { id: 'resp-1', textoRespuesta: 'Lado del cliente', isCorrect: true },
+      { id: 'resp-2', textoRespuesta: 'Servidor', isCorrect: false },
+      { id: 'resp-3', textoRespuesta: 'BIOS', isCorrect: false },
       {
-        id: "resp-4",
-        textoRespuesta: "Ninguna de las anteriores",
+        id: 'resp-4',
+        textoRespuesta: 'Ninguna de las anteriores',
         isCorrect: false,
       },
     ],
   },
   {
-    id: "2",
-    title: "¿Qué tipo de operador es (===)?",
+    id: '2',
+    title: '¿Qué tipo de operador es (===)?',
     options: [
-      { id: "resp-4", textoRespuesta: "Igualdad", isCorrect: false },
-      { id: "resp-5", textoRespuesta: "Igualdad estricta", isCorrect: true },
-      { id: "resp-6", textoRespuesta: "Desigualdad", isCorrect: false },
-      { id: "resp-7", textoRespuesta: "Diferente a", isCorrect: false },
+      { id: 'resp-4', textoRespuesta: 'Igualdad', isCorrect: false },
+      { id: 'resp-5', textoRespuesta: 'Igualdad estricta', isCorrect: true },
+      { id: 'resp-6', textoRespuesta: 'Desigualdad', isCorrect: false },
+      { id: 'resp-7', textoRespuesta: 'Diferente a', isCorrect: false },
     ],
   },
   {
-    id: "3",
-    title: "¿Cuánto es `11`+ 1 en JavaScript?",
+    id: '3',
+    title: '¿Cuánto es `11`+ 1 en JavaScript?',
     options: [
-      { id: "resp-8", textoRespuesta: "111", isCorrect: true },
-      { id: "resp-9", textoRespuesta: "12", isCorrect: false },
-      { id: "resp-10", textoRespuesta: "Syntax Error", isCorrect: false },
-      { id: "resp-11", textoRespuesta: "`11`1", isCorrect: false },
+      { id: 'resp-8', textoRespuesta: '111', isCorrect: true },
+      { id: 'resp-9', textoRespuesta: '12', isCorrect: false },
+      { id: 'resp-10', textoRespuesta: 'Syntax Error', isCorrect: false },
+      { id: 'resp-11', textoRespuesta: '`11`1', isCorrect: false },
     ],
   },
-  // {
-  //   title: '¿En qué año fue creado JavaScript?',
-  //   options: [
-  //     { textoRespuesta: '1997', isCorrect: false },
-  //     { textoRespuesta: '2001', isCorrect: false },
-  //     { textoRespuesta: '1987', isCorrect: false },
-  //     { textoRespuesta: '1995', isCorrect: true },
-  //   ],
-  // },
-  // {
-  //   title:
-  //     '¿Cuál es la sintaxis básica para declarar una variable en JavaScript?',
-  //   options: [
-  //     { textoRespuesta: 'var myVar;', isCorrect: true },
-  //     { textoRespuesta: 'variable myVar;', isCorrect: false },
-  //     { textoRespuesta: 'v = myVar;', isCorrect: false },
-  //     { textoRespuesta: 'declare myVar;', isCorrect: false },
-  //   ],
-  // },
-  // {
-  //   title: '¿Cuál es la diferencia entre == y === en JavaScript?',
-  //   options: [
-  //     {
-  //       textoRespuesta:
-  //         'Ambos operadores realizan una comparación estricta de igualdad.',
-  //       isCorrect: false,
-  //     },
-  //     {
-  //       textoRespuesta:
-  //         'El operador == compara el valor y el tipo de datos, mientras que === solo compara el valor.',
-  //       isCorrect: false,
-  //     },
-  //     {
-  //       textoRespuesta:
-  //         'El operador === compara el valor y el tipo de datos, mientras que == solo compara el valor.',
-  //       isCorrect: true,
-  //     },
-  //     {
-  //       textoRespuesta:
-  //         '== y === son lo mismo y se pueden usar indistintamente.',
-  //       isCorrect: false,
-  //     },
-  //   ],
-  // },
-  // {
-  //   title:
-  //     '¿Cuál de las siguientes opciones se usa para agregar un comentario de una sola línea en JavaScript?',
-  //   options: [
-  //     { textoRespuesta: '// Este es un comentario', isCorrect: true },
-  //     { textoRespuesta: '/* Este es un comentario */', isCorrect: false },
-  //     { textoRespuesta: '<!-- Este es un comentario -->', isCorrect: false },
-  //     { textoRespuesta: ' ** Este es un comentario **', isCorrect: false },
-  //   ],
-  // },
-  // {
-  //   title:
-  //     '¿Cuál es el método utilizado para agregar un elemento al final de un array en JavaScript?',
-  //   options: [
-  //     { textoRespuesta: 'push()', isCorrect: true },
-  //     { textoRespuesta: 'add()', isCorrect: false },
-  //     { textoRespuesta: 'append()', isCorrect: false },
-  //     { textoRespuesta: 'insert()', isCorrect: false },
-  //   ],
-  // },
-  // {
-  //   title:
-  //     '¿Cuál es la sintaxis correcta para imprimir un mensaje en la consola en JavaScript?',
-  //   options: [
-  //     { textoRespuesta: "print('Mensaje')", isCorrect: false },
-  //     { textoRespuesta: "console.log('Mensaje')", isCorrect: true },
-  //     { textoRespuesta: "log('Mensaje')", isCorrect: false },
-  //     { textoRespuesta: "display('Mensaje')", isCorrect: false },
-  //   ],
-  // },
-  // {
-  //   title:
-  //     '¿Cuál de las siguientes opciones es un método para convertir una cadena en minúsculas en JavaScript?',
-  //   options: [
-  //     { textoRespuesta: 'toLowerCase()', isCorrect: true },
-  //     { textoRespuesta: 'toUpperCase()', isCorrect: false },
-  //     { textoRespuesta: 'convertToLower()', isCorrect: false },
-  //     { textoRespuesta: 'convertToLowerCase()', isCorrect: false },
-  //   ],
-  // },
-  // {
-  //   title:
-  //     '¿Cuál es el resultado al ejecutar el siguiente código? console.log(typeof 42);',
-  //   options: [
-  //     { textoRespuesta: 'number', isCorrect: true },
-  //     { textoRespuesta: 'string', isCorrect: false },
-  //     { textoRespuesta: 'boolean', isCorrect: false },
-  //     { textoRespuesta: 'undefined', isCorrect: false },
-  //   ],
-  // },
-  // {
-  //   title:
-  //     '¿Cuál de los siguientes métodos se utiliza para eliminar el último elemento de un array en JavaScript?',
-  //   options: [
-  //     { textoRespuesta: 'pop()', isCorrect: true },
-  //     { textoRespuesta: 'remove()', isCorrect: false },
-  //     { textoRespuesta: 'delete()', isCorrect: false },
-  //     { textoRespuesta: 'splice()', isCorrect: false },
-  //   ],
-  // },
-  // {
-  //   title: '¿Cuál es la forma correcta de declarar una función en JavaScript?',
-  //   options: [
-  //     { textoRespuesta: 'function myFunction() {}', isCorrect: true },
-  //     { textoRespuesta: 'myFunction() = function() {}', isCorrect: false },
-  //     { textoRespuesta: 'def myFunction() {}', isCorrect: false },
-  //     { textoRespuesta: 'myFunction = () => {}', isCorrect: false },
-  //   ],
-  // },
+  {
+    id: '4',
+    title: '¿En qué año fue creado JavaScript?',
+    options: [
+      { id: 'resp-12', textoRespuesta: '1997', isCorrect: false },
+      { id: 'resp-13', textoRespuesta: '2001', isCorrect: false },
+      { id: 'resp-14', textoRespuesta: '1987', isCorrect: false },
+      { id: 'resp-15', textoRespuesta: '1995', isCorrect: true },
+    ],
+  },
+  {
+    id: '5',
+    title:
+      '¿Cuál es la sintaxis básica para declarar una variable en JavaScript?',
+    options: [
+      { id: 'resp-16', textoRespuesta: 'var myVar;', isCorrect: true },
+      { id: 'resp-17', textoRespuesta: 'variable myVar;', isCorrect: false },
+      { id: 'resp-18', textoRespuesta: 'v = myVar;', isCorrect: false },
+      { id: 'resp-19', textoRespuesta: 'declare myVar;', isCorrect: false },
+    ],
+  },
+  {
+    id: '6',
+    title: '¿Cuál es la diferencia entre == y === en JavaScript?',
+    options: [
+      {
+        id: 'resp-20',
+        textoRespuesta:
+          'Ambos operadores realizan una comparación estricta de igualdad.',
+        isCorrect: false,
+      },
+      {
+        id: 'resp-21',
+        textoRespuesta:
+          'El operador == compara el valor y el tipo de datos, mientras que === solo compara el valor.',
+        isCorrect: false,
+      },
+      {
+        id: 'resp-22',
+        textoRespuesta:
+          'El operador === compara el valor y el tipo de datos, mientras que == solo compara el valor.',
+        isCorrect: true,
+      },
+      {
+        id: 'resp-23',
+        textoRespuesta:
+          '== y === son lo mismo y se pueden usar indistintamente.',
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    id: '5',
+    title:
+      '¿Cuál de las siguientes opciones se usa para agregar un comentario de una sola línea en JavaScript?',
+    options: [
+      {
+        id: 'resp-24',
+        textoRespuesta: '// Este es un comentario',
+        isCorrect: true,
+      },
+      {
+        id: 'resp-25',
+        textoRespuesta: '/* Este es un comentario */',
+        isCorrect: false,
+      },
+      {
+        id: 'resp-26',
+        textoRespuesta: '<!-- Este es un comentario -->',
+        isCorrect: false,
+      },
+      {
+        id: 'resp-27',
+        textoRespuesta: ' ** Este es un comentario **',
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    id: '7',
+    title:
+      '¿Cuál es el método utilizado para agregar un elemento al final de un array en JavaScript?',
+    options: [
+      { id: 'resp-28', textoRespuesta: 'push()', isCorrect: true },
+      { id: 'resp-29', textoRespuesta: 'add()', isCorrect: false },
+      { id: 'resp-30', textoRespuesta: 'append()', isCorrect: false },
+      { id: 'resp-31', textoRespuesta: 'insert()', isCorrect: false },
+    ],
+  },
+  {
+    id: '8',
+    title:
+      '¿Cuál es la sintaxis correcta para imprimir un mensaje en la consola en JavaScript?',
+    options: [
+      { id: 'resp-32', textoRespuesta: "print('Mensaje')", isCorrect: false },
+      {
+        id: 'resp-33',
+        textoRespuesta: "console.log('Mensaje')",
+        isCorrect: true,
+      },
+      { id: 'resp-34', textoRespuesta: "log('Mensaje')", isCorrect: false },
+      { id: 'resp-35', textoRespuesta: "display('Mensaje')", isCorrect: false },
+    ],
+  },
+  {
+    id: '9',
+    title:
+      '¿Cuál de las siguientes opciones es un método para convertir una cadena en minúsculas en JavaScript?',
+    options: [
+      { id: 'resp-36', textoRespuesta: 'toLowerCase()', isCorrect: true },
+      { id: 'resp-37', textoRespuesta: 'toUpperCase()', isCorrect: false },
+      { id: 'resp-38', textoRespuesta: 'convertToLower()', isCorrect: false },
+      {
+        id: 'resp-39',
+        textoRespuesta: 'convertToLowerCase()',
+        isCorrect: false,
+      },
+    ],
+  },
+  {
+    id: '10',
+    title:
+      '¿Cuál es el resultado al ejecutar el siguiente código? console.log(typeof 42);',
+    options: [
+      { id: 'resp-40', textoRespuesta: 'number', isCorrect: true },
+      { id: 'resp-41', textoRespuesta: 'string', isCorrect: false },
+      { id: 'resp-42', textoRespuesta: 'boolean', isCorrect: false },
+      { id: 'resp-43', textoRespuesta: 'undefined', isCorrect: false },
+    ],
+  },
+  {
+    id: '11',
+    title:
+      '¿Cuál de los siguientes métodos se utiliza para eliminar el último elemento de un array en JavaScript?',
+    options: [
+      { id: 'resp-44', textoRespuesta: 'pop()', isCorrect: true },
+      { id: 'resp-45', textoRespuesta: 'remove()', isCorrect: false },
+      { id: 'resp-46', textoRespuesta: 'delete()', isCorrect: false },
+      { id: 'resp-47', textoRespuesta: 'splice()', isCorrect: false },
+    ],
+  },
+  {
+    id: '12',
+    title: '¿Cuál es la forma correcta de declarar una función en JavaScript?',
+    options: [
+      {
+        id: 'resp-48',
+        textoRespuesta: 'function myFunction() {}',
+        isCorrect: true,
+      },
+      {
+        id: 'resp-49',
+        textoRespuesta: 'myFunction() = function() {}',
+        isCorrect: false,
+      },
+      {
+        id: 'resp-50',
+        textoRespuesta: 'def myFunction() {}',
+        isCorrect: false,
+      },
+      {
+        id: 'resp-51',
+        textoRespuesta: 'myFunction = () => {}',
+        isCorrect: false,
+      },
+    ],
+  },
 ];
